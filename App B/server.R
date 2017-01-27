6f691249f08f5bf473682a328e0dffeb7e33fd54
carlganz <carlganz@gmail.com>
2017-01-27 23:10:55 +0000

    App B sort of works

--- App B/server.R
+++ App B/server.R
@@ -1,11 +1,81 @@
 library(shiny)
 library(DT)
-library(rintrojs)
 library(dplyr)
+library(stringdist)
 
 
 server <- function(input, output, session) {
 
-  chis <- readRDS("../Data/chis.rds")
+  # helper function for making inputs
+  # in datatable
+  # stolen from Yihui somewhere
+  shinyInput = function(FUN, len, id, ...) {
+    inputs = character(len)
+    for (i in seq_len(len)) {
+      inputs[i] = as.character(FUN(paste0(id, i), label = NULL, ...))
+    }
+    inputs
+  }
+
+  output$formats <- renderTable({
+    req(input$var)
+    data.frame(
+      values = c(-1, 1:7),
+      formats = c(
+        "Inapplicable",
+        "North America",
+        "South America",
+        "Asia",
+        "Europe",
+        "Africa",
+        "Oceania",
+        "Other"
+      )
+    )
+  })
+
+  upcodes <- reactive({
+    readRDS("../Data/responses.rds")
+  })
+
+  pastUpcodes <- reactive({
+    readRDS("../Data/pastUpcodes.rds")
+  })
+
+  output$responses <- DT::renderDataTable({
+    req(input$var)
+
+    compare <- upcodes()
+    pastUpcodes <- pastUpcodes()
+    compare[,"SimilarResponse"] <- NA
+    compare[,"SimilarResponseUpcode"] <- NA
+
+    for (i in seq_len(nrow(compare))) {
+      response <- compare[i,"answer"]
+
+      comp <- stringdist(response,
+                         pastUpcodes$answer)
+      if (any(comp < 3)) {
+        best <- which(comp==min(comp))
+        compare[i, "SimilarResponse"] <- pastUpcodes$answer[best]
+        compare[i, "SimilarResponseUpcode"] <-
+          pastUpcodes$upcode[best]
+      }
+
+    }
+
+    data.frame(compare,
+               inputUpcode =
+                 shinyInput(numericInput, nrow(compare),
+                            "upcodeVal", value = NULL)
+                 )
+  },
+  rownames = FALSE, selection = list(mode="none"),
+  escape = c(-5), options = list(
+    autoWidth = TRUE,
+    columnDefs = list(list(
+      width = "20px", target = 3
+    ))
+  ))
 
 }
--- App B/ui.R
+++ App B/ui.R
@@ -1,12 +1,35 @@
 library(shiny)
 library(DT)
-library(rintrojs)
 
 ui <- fluidPage(
-  introjsUI(),
   titlePanel("App B"),
   sidebarLayout(
-    sidebarPanel(),
-    mainPanel()
+    sidebarPanel(
+      selectizeInput(
+        "var", "Select a variable to upcode",
+        choices = c("continent_of_origin"),
+        options = list(
+          onInitialize = I('function() { this.setValue(""); }')
+        )
+      )
+    ),
+    mainPanel(
+      conditionalPanel(
+        condition = 'input.var !== "" & input.var !== null',
+        h2("Contintent of Origin"),
+        fluidRow(
+          column(width = 5,
+          p("On the right are the set levels for the
+            contitent of origin variable. Below is a table
+            of answers respondents gave when they were asked their
+            country of origin. Please categorize the responses to
+            the correct continent. ")),
+          column(width = 7,
+          tableOutput("formats")
+          )
+        ),
+        DT::dataTableOutput("responses")
+      )
+    )
   )
 )
